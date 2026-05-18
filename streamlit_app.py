038735e24b6adab3b92a1304405fdaa066fa3b9b
Rohit Malode <rohitmalode10@gmail.com>
2026-05-18 20:23:29 +0530

    Enhance fruit data retrieval and API request
    Updated the fruit data retrieval to include the SEARCH_ON column and modified the API request to use the search value for each chosen fruit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # App Title
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -17,18 +18,16 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-# Get Fruit Data
-my_dataframe = session.table(
-    "smoothies.public.fruit_options"
-).select(col('FRUIT_NAME'))
+# Get Fruit Data (including the new SEARCH_ON column)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
-# Fruit List
-fruit_list = my_dataframe.to_pandas()['FRUIT_NAME'].tolist()
+# Convert the Snowpark Dataframe to a Pandas Dataframe
+pd_df = my_dataframe.to_pandas()
 
-# Multiselect
+# Multiselect using the FRUIT_NAME column from our Pandas Dataframe
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
-    fruit_list,
+    pd_df['FRUIT_NAME'],
     max_selections=5
 )
 
@@ -39,8 +38,12 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         
+        # Look up the SEARCH_ON value for the chosen fruit
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(
             data=smoothiefroot_response.json(),
             use_container_width=True
